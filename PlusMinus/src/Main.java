51c5fd881ae0a9f00b4a246aa77c071c97c33d74
Michal <michal.ligocki@wp.pl>
2022-05-19 18:34:35 +0200

    add README.md

---
+++ PlusMinus/src/Main.java
@@ -0,0 +1,49 @@
+import java.sql.SQLOutput;
+import java.util.*;
+
+class Result {
+
+    public static void plusMinus(List<Integer> arr) {
+        float pluses = 0;
+        float minuses = 0;
+        float zeros = 0;
+        int arrLength = arr.size();
+
+        for (int number : arr) {
+            if (number > 0) {
+                pluses++;
+            } else if (number < 0) {
+                minuses++;
+            } else {
+                zeros++;
+            }
+        }
+        System.out.printf("%.6f %n", pluses / arrLength);
+        System.out.printf("%.6f %n", minuses / arrLength);
+        System.out.printf("%.6f %n", zeros / arrLength);
+
+    }
+
+}
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Integer> testInput = new ArrayList<Integer>();
+        testInput.add(-4);
+        testInput.add(3);
+        testInput.add(-9);
+        testInput.add(0);
+        testInput.add(4);
+        testInput.add(1);
+
+        System.out.println(testInput);
+
+
+        Result.plusMinus(testInput);
+
+
+    }
+}
